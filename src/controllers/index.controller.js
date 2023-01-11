5c5897ae104db5b6687241ffb879196356886dd3
Victor Tique <vtique@kinpos.com>
2023-01-11 13:19:52 -0500

    Ajustes peticiones

--- src/controllers/index.controller.js
+++ src/controllers/index.controller.js
@@ -1,6 +1,8 @@
 import {pool} from "../db.js";
 
+export const index = (req, res) => res.json({message: 'welcome to my api'});
+
 export const ping = async (req, res) => {
-    const [resul] = await pool.query('SELECT 1 + 1 AS result')
+    const [resul] = await pool.query('SELECT "pong" as result')
     res.json(resul[0])
 }
